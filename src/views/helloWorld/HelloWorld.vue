348c1be605f79ce45e39a28f20024f63dcab0308
kangqi0421 <33271323+kangqi0421@users.noreply.github.com>
2020-02-18 15:16:27 +0800

    Update HelloWorld.vue

--- src/views/helloWorld/HelloWorld.vue
+++ src/views/helloWorld/HelloWorld.vue
@@ -2,9 +2,5 @@
     <div>
 <<<<<<< HEAD
        this is test!
-=======
-        Hello!test! 20200218-1026 Hello!test! 20200218-1026 Hello!test! 20200218-1026
-        Hello!test! 20200218-1026 Hello!test! 20200218-1026 Hello!test! 20200218-1026
->>>>>>> f5f85f28ac8ea074f9d01675d5851407088fd303
     </div>
 </template>
