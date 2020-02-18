e58d594386fa59e0b6264d3fc6892ee7a199dbcd
hyr <seerhyr@163.com>
2020-02-18 16:02:07 +0800

    aa

--- src/views/helloWorld/HelloWorld.vue
+++ src/views/helloWorld/HelloWorld.vue
@@ -1,10 +1,6 @@
 <template>
     <div>
-<<<<<<< HEAD
-       this is test!
-=======
-        Hello!test! 20200218-1026 Hello!test! 20200218-1026 Hello!test! 20200218-1026
-        Hello!test! 20200218-1026 Hello!test! 20200218-1026 Hello!test! 20200218-1026
->>>>>>> f5f85f28ac8ea074f9d01675d5851407088fd303
+       this is test! 202002181601!
     </div>
 </template>
+
